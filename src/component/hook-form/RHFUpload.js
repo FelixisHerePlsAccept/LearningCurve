f9749e126c437042a069e60e45ec18e4f8aaeb50
Feel Ick <134363757+FelixisHerePlsAccept@users.noreply.github.com>
2024-06-28 20:46:15 +0800

    ADD: component

---
+++ src/component/hook-form/RHFUpload.js
@@ -0,0 +1,173 @@
+import PropTypes from 'prop-types';
+// form
+import { useFormContext, Controller } from 'react-hook-form';
+// @mui
+import { FormHelperText, Typography } from '@mui/material';
+//
+import UploadBox from '../upload/UploadBox'
+// import { UploadAvatar, Upload, UploadBox,UploadSquare} from '../upload';
+
+// ----------------------------------------------------------------------
+
+// RHFUploadAvatar.propTypes = {
+//   name: PropTypes.string,
+// };
+
+// ----------------------------------------------------------------------
+
+// export function RHFUploadAvatar({ name, ...other }) {
+//   const { control } = useFormContext();
+
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       render={({ field, fieldState: { error } }) => (
+//         <div>
+//           <UploadAvatar
+//             accept={{
+//               'image/*': [],
+//             }}
+//             error={!!error}
+//             file={field.value}
+//             {...other}
+//           />
+
+//           {!!error && (
+//             <FormHelperText error sx={{ px: 2, textAlign: 'center' }}>
+//               {error.message}
+//             </FormHelperText>
+//           )}
+//         </div>
+//       )}
+//     />
+//   );
+// }
+
+// ----------------------------------------------------------------------
+
+RHFUploadBox.propTypes = {
+  name: PropTypes.string,
+};
+
+export function RHFUploadBox({ name, ...other }) {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+
+        <UploadBox files={field.value} error={!!error} {...other} />
+        
+      )}
+    />
+  );
+}
+
+// ----------------------------------------------------------------------
+
+// RHFUpload.propTypes = {
+//   name: PropTypes.string,
+//   multiple: PropTypes.bool,
+//   helperText: PropTypes.node,
+// };
+
+// export function RHFUpload({ name, multiple, helperText, ...other }) {
+//   const { control } = useFormContext();
+
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       render={({ field, fieldState: { error } }) =>
+//         multiple ? (
+//             <Upload
+//             multiple
+//             accept={{ 'image/*': [] }}
+//             files={field.value}
+//             error={!!error}
+//             helperText={
+//               (!!error || helperText) && (
+//                 <FormHelperText error={!!error} sx={{ px: 2 }}>
+//                   {error ? error?.message : helperText}
+//                 </FormHelperText>
+//               )
+//             }
+//             {...other}
+//           />
+        
+
+//         ) : (
+//           <Upload
+//             accept={{ 'image/*': [] }}
+//             file={field.value}
+//             error={!!error}
+//             helperText={
+//               (!!error || helperText) && (
+//                 <FormHelperText error={!!error} sx={{ px: 2 }}>
+//                   {error ? error?.message : helperText}
+//                 </FormHelperText>
+//               )
+//             }
+//             {...other}
+//           />
+//         )
+//       }
+//     />
+//   );
+// }
+
+
+// RHFUploadSquare.propTypes = {
+//   name: PropTypes.string,
+//   multiple: PropTypes.bool,
+//   helperText: PropTypes.node,
+// };
+
+// export function RHFUploadSquare({ name, multiple, helperText, ...other }) {
+//   const { control } = useFormContext();
+
+//   return (
+//     <Controller
+//       name={name}
+//       control={control}
+//       render={({ field, fieldState: { error } }) =>
+//         multiple ? (
+//             <UploadSquare
+//             multiple
+//             accept={{ 'image/*': [] }}
+//             files={field.value}
+//             error={!!error}
+//             helperText={
+//               (!!error || helperText) && (
+//                 <FormHelperText error={!!error} sx={{ px: 2 }}>
+//                   {error ? error?.message : helperText}
+//                 </FormHelperText>
+//               )
+//             }
+//             {...other}
+//           />
+        
+
+//         ) : (
+//           <UploadSquare
+//             accept={{ 'image/*': [] }}
+//             file={field.value}
+//             error={!!error}
+//             helperText={
+//               (!!error || helperText) && (
+//                 <FormHelperText error={!!error} sx={{ px: 2 }}>
+//                   {error ? error?.message : helperText}
+//                 </FormHelperText>
+//               )
+//             }
+//             {...other}
+//           />
+//         )
+//       }
+//     />
+//   );
+// }
+
---
+++ src/component/table/TableToolbar.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+// @mui
+import { Stack, InputAdornment, TextField, MenuItem, Button } from '@mui/material';
+// components
+// import Iconify from '../iconify';
+
+// ----------------------------------------------------------------------
+
+TableToolbar.propTypes = {
+  isFiltered: PropTypes.bool,
+  filterName: PropTypes.string,
+  onFilterName: PropTypes.func,
+  onResetFilter: PropTypes.func,
+  optionsRole: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default function TableToolbar({
+  isFiltered,
+  filterName,
+  optionsRole,
+  onFilterName,
+  onResetFilter,
+}) {
+  return (
+    <Stack
+      spacing={2}
+      alignItems="center"
+      direction={{
+        xs: 'column',
+        sm: 'row',
+      }}
+      sx={{ px: 2.5, py: 3 }}
+    >
+      <TextField
+        fullWidth
+        value={filterName}
+        onChange={onFilterName}
+        placeholder="Search..."
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              {/* <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled' }} /> */}
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      {isFiltered && (
+        <Button
+          color="error"
+          sx={{ flexShrink: 0 }}
+          onClick={onResetFilter}
+          // startIcon={<Iconify icon="eva:trash-2-outline" />}
+        >
+          Clear
+        </Button>
+      )}
+    </Stack>
+  );
+}
