acd93abbb7e5dd9c63b6dfa0a5e8665fd8a788f9
Feel Ick <134363757+FelixisHerePlsAccept@users.noreply.github.com>
2024-08-09 17:23:39 +0800

    Final Change #1

--- src/page/login.js
+++ src/page/login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo, useState } from 'react'
+import React, { useContext, useEffect, useMemo, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { LoadingButton } from '@mui/lab'
 import { signInWithEmailAndPassword } from 'firebase/auth'
@@ -22,7 +22,11 @@ export default function Login() {
     const {dispatch, currentUser} = useContext(AuthContext)
     const {isMobile} = useContext(MediaContext)
 
-    console.log(isMobile ? 'isMobile' : 'isNotMobile')
+    useEffect (() => {
+        if(currentUser){
+            navigate(PATH_MAIN.datalist)
+        }
+    },[currentUser, navigate])
 
     const defaultValue = useMemo (
         () => ({
@@ -124,70 +128,66 @@ export default function Login() {
         )
     }
 
-    if (currentUser) {
-        navigate(PATH_MAIN.datalist)
-    } else {
-        return (
-            <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+    return (
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+            <Box 
+                sx={{
+                    display:'flex',
+                    justifyContent:'center',
+                    alignItems:'center',
+                    width:'100%', 
+                    height:'100vh', 
+                    bgcolor:'red'
+                }}
+            >
                 <Box 
                     sx={{
-                        display:'flex',
-                        justifyContent:'center',
-                        alignItems:'center',
-                        width:'100%', 
-                        height:'100vh', 
-                        bgcolor:'red'
+                        height:'auto',
+                        width:'30%',
+                        bgcolor:'white',
+                        p:'1rem 0 1rem 0',
+                        borderRadius:'1rem'
                     }}
                 >
-                    <Box 
-                        sx={{
-                            height:'auto',
-                            width:'30%',
-                            bgcolor:'white',
-                            p:'1rem 0 1rem 0',
-                            borderRadius:'1rem'
-                        }}
-                    >
-                        <Stack spacing={2} sx={{width:'50%', margin:'auto'}}>
-                            <Typography variant='h4' fontWeight={700} textAlign={'center'}>
-                                Bonjour Monsieur
+                    <Stack spacing={2} sx={{width:'50%', margin:'auto'}}>
+                        <Typography variant='h4' fontWeight={700} textAlign={'center'}>
+                            Bonjour Monsieur
+                        </Typography>
+                        <Typography variant='h6' fontWeight={500} textAlign={'center'}>
+                            Login
+                        </Typography>
+                        <RHFTextField name="email" showLabel={true} />
+                        <RHFTextField 
+                            name="password" 
+                            type={showPassword ? 'text' :  'password'}
+                            autoComplete='off'
+                            showLabel={true}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position="end">
+                                        <IconButton onClick={() => setShowPassword(prev => !prev)}>
+                                            {showPassword ? <EyeIcon style={{width:'1.5rem', height:'1.5rem'}} /> : <EyeSlashIcon style={{width:'1.5rem', height:'1.5rem'}} />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                        <LoadingButton type="submit" variant="contained" loading={ isSubmitting }>
+                            Login
+                        </LoadingButton>
+                        <Stack direction='row' sx={{justifyContent:'center', alignItems:'center'}} spacing={1}>
+                        <Typography variant='subtitle2'>
+                                Not a gooner?
                             </Typography>
-                            <Typography variant='h6' fontWeight={500} textAlign={'center'}>
-                                Login
-                            </Typography>
-                            <RHFTextField name="email" showLabel={true} />
-                            <RHFTextField 
-                                name="password" 
-                                type={showPassword ? 'text' :  'password'}
-                                autoComplete='off'
-                                showLabel={true}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment position="end">
-                                            <IconButton onClick={() => setShowPassword(prev => !prev)}>
-                                                {showPassword ? <EyeIcon style={{width:'1.5rem', height:'1.5rem'}} /> : <EyeSlashIcon style={{width:'1.5rem', height:'1.5rem'}} />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    )
-                                }}
-                            />
-                            <LoadingButton type="submit" variant="contained" loading={ isSubmitting }>
-                                Login
-                            </LoadingButton>
-                            <Stack direction='row' sx={{justifyContent:'center', alignItems:'center'}} spacing={1}>
-                            <Typography variant='subtitle2'>
-                                    Not a gooner?
+                            <a href='/signup'>
+                                <Typography variant='subtitle2'>
+                                    Sign here
                                 </Typography>
-                                <a href='/signup'>
-                                    <Typography variant='subtitle2'>
-                                        Sign here
-                                    </Typography>
-                                </a>
-                            </Stack>
+                            </a>
                         </Stack>
-                    </Box>
+                    </Stack>
                 </Box>
-            </FormProvider>
-        )
-    }
+            </Box>
+        </FormProvider>
+    )
 }
